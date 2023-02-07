42887f1f092e7f2d45717e7eb8b429a1395ad368
Caroline Oliveira <79587409+carolinevsboliveira@users.noreply.github.com>
2023-02-06 22:57:58 -0300

    test: check funcionality (#6)

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -6,25 +6,106 @@ const cli = async (cmd) =>
   system.run(
     'node ' + filesystem.path(src, 'bin', 'react-modest-cli') + ` ${cmd}`
   )
+describe('react-modest-cli', () => {
+  describe('help commands', () => {
+    it('outputs version', async () => {
+      const output = await cli('--version')
+      expect(output).toContain('0.0.1')
+    })
 
-test('outputs version', async () => {
-  const output = await cli('--version')
-  expect(output).toContain('0.0.1')
-})
+    it('outputs help', async () => {
+      const output = await cli('--help')
+      expect(output).toContain('0.0.1')
+    })
+  })
 
-test('outputs help', async () => {
-  const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
+  describe('create files command', () => {
+    describe('when user types invalid parameters', () => {
+      it('should validate missing name before generate files', async () => {
+        const output = await cli('create')
 
-test('generates file', async () => {
-  const output = await cli('generate foo')
+        expect(output).toContain('A component name should be provided.')
+      })
 
-  expect(output).toContain('Generated file at models/foo-model.ts')
-  const foomodel = filesystem.read('models/foo-model.ts')
+      it('should validate missing type before generate files', async () => {
+        const output = await cli('create first-component js')
 
-  expect(foomodel).toContain(`module.exports = {`)
-  expect(foomodel).toContain(`name: 'foo'`)
+        expect(output).toContain(
+          'Invalid type paramter. Try one of these keys: css, styled, scss, single.'
+        )
+      })
 
-  filesystem.remove('models')
+      it('should validate wrong type before generate files', async () => {
+        const output = await cli('create first-component js --type=fake')
+
+        expect(output).toContain(
+          'Invalid type paramter. Try one of these keys: css, styled, scss, single.'
+        )
+      })
+
+      it('should validate wrong language before validate files', async () => {
+        const output = await cli('create ys first-component --type=fake')
+
+        expect(output).toContain(
+          'Unsuported language. Please insert one of these keys: ts, typescript, js, javascript.'
+        )
+      })
+    })
+
+    describe('when user types correct params', () => {
+      it('should generate correct js template for single', async () => {
+        const output = await cli('create card js --type=single')
+
+        expect(output).toContain(
+          'Template generated successfully at folder src/components'
+        )
+        const singleModel = filesystem.read('src/components/card.jsx')
+        expect(singleModel).toBeTruthy()
+
+        filesystem.remove('src/components/')
+      })
+
+      it('should generate correct js template for css', async () => {
+        const output = await cli('create card js --type=css')
+
+        expect(output).toContain(
+          'Template generated successfully at folder src/components'
+        )
+        const singleModel = filesystem.read('src/components/card.jsx')
+        const singleModelCss = filesystem.read('src/components/style.css')
+
+        expect(singleModel).toBeTruthy()
+        expect(singleModelCss).toBeTruthy()
+
+        filesystem.remove('src/components/')
+      })
+
+      it('should generate correct ts template for single', async () => {
+        const output = await cli('create card ts --type=single')
+
+        expect(output).toContain(
+          'Template generated successfully at folder src/components'
+        )
+        const singleModel = filesystem.read('src/components/card.tsx')
+        expect(singleModel).toBeTruthy()
+
+        filesystem.remove('src/components/')
+      })
+
+      it('should generate correct js template for css', async () => {
+        const output = await cli('create card ts --type=css')
+
+        expect(output).toContain(
+          'Template generated successfully at folder src/components'
+        )
+        const singleModel = filesystem.read('src/components/card.tsx')
+        const singleModelCss = filesystem.read('src/components/style.css')
+
+        expect(singleModel).toBeTruthy()
+        expect(singleModelCss).toBeTruthy()
+
+        filesystem.remove('src/components/')
+      })
+    })
+  })
 })
--- src/helpers/params-validation.ts
+++ src/helpers/params-validation.ts
@@ -13,21 +13,20 @@ export function validateInputedParams({
 }: validateInputedParamsProps): void {
   const supportedLanguages = ['ts', 'typescript', 'js', 'javascript']
 
+  if (!generatedName) throw new Error('A component name should be provided.')
+
   if (!supportedLanguages.includes(language))
     throw new Error(
       `Unsuported language. Please insert one of these keys: ${supportedLanguages.join(
         ', '
       )}.`
     )
+  const validTypes = Array.from(template(language).keys())
 
-  if (!generatedName) throw new Error('A component name should be provided.')
-
-  const typeTranslator = template[language].get(type)
-
-  if (!typeTranslator)
+  if (!type || !validTypes.includes(type))
     throw new Error(
       `Invalid type paramter. Try one of these keys: ${Array.from(
-        typeTranslator.keys()
+        validTypes
       ).join(', ')}.`
     )
 }
